10cbeb73d539854f2bd0fb017d9a82161b38cf28
Jose Ricardo Mendoza Villar <richi_1325@hotmail.com>
2022-04-28 15:45:34 -0500

    feature: SVC  baseline transformation AutorshipVerification to StyleChanging

--- baseline_methods/SupportVectorClassification/README.md
+++ baseline_methods/SupportVectorClassification/README.md
@@ -1,3 +1,26 @@
 # Authorship Verification Using SVC
 
-### En modificaciones
+***  
+## Ejecutar
+  
+Para ejecutar el script, usando los datos del pan22 ya preparados (revisar carpeta `corpus` en la raíz del proyecto):
+
+```sh
+python3 main.py -input_pairs="../../corpus/pan22/dataset1/prepared/train/train.jsonl" \
+    -input_truth="../../corpus/pan22/dataset1/prepared/train/train_truth.jsonl" \ 
+    -test_pairs="../../corpus/pan22/dataset1/prepared/validation/validation.json" \
+    -test_truth="../../corpus/pan22/dataset1/validation"
+```
+
+Salida:
+El programa guarda dentro de la carpeta `prediction` todas las prediciones con el nombre `prediction-problem-*.json` donde `*` corresponde al número del documento predicho y además cálcula el valor de la métrica solicitada por el PAN guardadolo en la carpeta `resultados/SupportVectorClassification` que se encuentra en la raíz del proyecto.
+
+
+### Argumentos de `main.py`
+
+| Args   		  | Description                                                              |
+|-----------------|--------------------------------------------------------------------------|
+| `-input_pairs`  | Archivo .jsonl procesado para el entrenamiento                           |
+| `-input_truth`  | Archivo .jsonl procesado que contiene las etiquetas del entrenamiento    |
+| `-test_pairs`   | Archivo .json procesado que contiene los pares de textos para validación |
+| `-test_truth`   | Ruta donde se encuentran las etiquetas para el conjunto de validación    |
--- baseline_methods/SupportVectorClassification/main.py
+++ baseline_methods/SupportVectorClassification/main.py
@@ -1,4 +1,5 @@
 import os
+import platform
 import re
 import numpy as np
 import argparse
@@ -18,103 +19,64 @@ except:
 
 def svcBinaryClassifier():
     parser = argparse.ArgumentParser(description='SVC script AA@PAN')
-    parser.add_argument('-t', type=str,
-        help='Path to the jsonl-file with the text test dataset')
-    parser.add_argument('-v', type=str,
-        help='Path to the jsonl-file with ground test truth scores')
-    parser.add_argument('-n', type=str,
-        help='Path to the jsonl-file with the text train dataset')
-    parser.add_argument('-y', type=str,
-        help='Path to the jsonl-file with ground train truth scores')
+    parser.add_argument('-train_pairs', type=str, required=True,
+                        help='Path to the jsonl-file with the train pairs')
+    parser.add_argument('-train_truth', type=str, required=True,
+                        help='Path to the ground truth jsonl-file for the train pairs')
+    parser.add_argument('-test_pairs', type=str, required=True,
+                        help='Path to the json-file with the validation/test pairs')
+    parser.add_argument('-test_truth', type=str, required=True,
+                        help='Path to the ground truth-file for the validation folder')
     args = parser.parse_args()
 
-    if not args.v:
-        raise ValueError('The ground test truth path is required')
-    if not args.t:
-        raise ValueError('The test dataset is required')
-    if not args.y:
-        raise ValueError('The ground truth train path is required')
-    if not args.n:
-        raise ValueError('The train dataset is required')
+    BASE_DIR = Path(__file__).resolve().parent
 
-    data = pd.DataFrame(getDataJSON(args.n)).set_index("id")
+    data = pd.DataFrame(getDataJSON(args.train_pairs)).set_index("id")
     data[['text1','text2']] = pd.DataFrame(data.pair.tolist(), index= data.index)
 
     del data["pair"]
-    data2 = pd.DataFrame(getDataJSON(args.y)).set_index("id")
+    data2 = pd.DataFrame(getDataJSON(args.train_truth)).set_index("id")
     data = pd.merge(data,data2,how='outer',left_index=True,right_index=True)
     del data2
 
     unigram_vectorizer = CountVectorizer(ngram_range=(1, 1), token_pattern=r'\b\w+\b', min_df=1)
     X_2 = unigram_vectorizer.fit_transform(data["text1"])
     X_2 = X_2.toarray() - unigram_vectorizer.transform(data["text2"]).toarray()
+    Y = data["value"]
 
     #Entrenamiento SVC
     #----------------------------------kernel lineal
-<<<<<<< HEAD
-    clf = SVC(kernel='linear', probability=True, tol=1e-3)
+    clf = SVC(kernel='linear', probability=True)
+    clf.fit(X_2, Y)    
 
-    X_2 = X_2[:200]
-    Y = data["value"][:200]
+    #Conjunto de test/validation
+    with open(args.test_pairs) as f:
+        data = json.load(f)
+    for dict in data:
+        pred = []
+        conf = []
+        for dict_line in dict:
+            X_test = unigram_vectorizer.transform([dict_line["pair"][0]]).toarray() -\
+                     unigram_vectorizer.transform([dict_line["pair"][1]]).toarray()
+            pred.append(clf.predict_proba(X_test)[0][1])
+            conf.append(0)
 
-    clf.fit(X_2, Y)
-=======
-    clf = SVC(gamma='auto',probability=True)
-    clf.fit(X_2, data["value"])
->>>>>>> f907e7a1da94aed453e49d97b8da655110d8ee09
+        conf[np.argmin(pred)] = 1
+        problem_number = int(re.findall(r'([0-9]+)-[0-9]+-[0-9]+',dict_line["id"])[0])
+        with open(os.path.join(BASE_DIR,"prediction",f"prediction-problem-{problem_number}.json"),"w+") as f:
+            json.dump({"changes":conf},f)
     
-    data_test = pd.DataFrame(getDataJSON(args.t)).set_index("id")
-    data_test[['text1','text2']] = pd.DataFrame(data_test.pair.tolist(), index= data_test.index)
-    del data_test["pair"]
-    data2 = pd.DataFrame(getDataJSON(args.v)).set_index("id")
-    data_test = pd.merge(data_test,data2,how='outer',left_index=True,right_index=True)
-    del data2
+    PREDICTION_DIR = os.path.join(BASE_DIR,".","prediction") 
+    OUTPUT_DIR = os.path.join(BASE_DIR,"..","..","resultados","CosineSimilarity")
 
-    #Predicción
-    X_test = unigram_vectorizer.transform(data_test["text1"]).toarray() -\
-            unigram_vectorizer.transform(data_test["text2"]).toarray()
-    y_pred = clf.predict_proba(X_test)
-
-    BASE_DIR = Path(__file__).resolve().parent
-    predictions = []
-    aux = []
-    inicial = re.findall(r'([0-9]+)-[0-9]+-[0-9]+',data_test.index[0])[0]
-    for x,y in zip(data_test.index,y_pred):
-        actual = re.findall(r'([0-9]+)-[0-9]+-[0-9]+',x)[0]
-        index = re.findall(r'[0-9]+-([0-9]+)-[0-9]+',x)[0]
-        print(inicial,actual)
-        if inicial == actual:
-            aux.append({"index":index,"aux":x, "value":y[0]})
-        else:
-            predictions.append(aux)
-            aux = []
-            aux.append({"index":index,"aux":x, "value":y[0]})
-            inicial = actual
-    
-    j=0
-    for text in predictions:
-        max_proba = text[0]["value"]
-        max_index = text[0]["index"]
-        for prediction in text:
-            if max_proba < prediction["value"]:
-                max_proba = prediction["value"]
-                max_index = prediction["index"]
-        len_index = int(prediction["index"])
-        
-        if len_index == 0:
-            changes = [1]
-        else:
-            changes = []
-            for i in range(len_index):
-                if i==0 and int(max_index) == 0:
-                    changes.append(1)
-                elif i == int(max_index)-1:
-                    changes.append(1)
-                changes.append(0)
-        with open(os.path.join(BASE_DIR,"prediction",f"prediction-problem-{re.findall(r'([0-9]+)-[0-9]+-[0-9]+',text[0]['aux'])[0]}.json"),"w+") as f:
-            f.write(str({"changes":changes}).replace("'[","[").replace("']","]").replace("'",'"'))
-        print("Predictions saved in:")
-        print(os.path.join(BASE_DIR,"prediction"))                        
+    if "Windows" in platform.system():
+        subprocess.run(["python","../../utils/evaluator_2022.py","-p",
+            PREDICTION_DIR,"-t",args.test_truth,"-o",
+            OUTPUT_DIR], capture_output=True)
+    else:
+        subprocess.run(["python3","../../utils/evaluator_2022.py","-p",
+            PREDICTION_DIR,"-t",args.test_truth,"-o",
+            OUTPUT_DIR], capture_output=True)
             
 if __name__ == "__main__":
     svcBinaryClassifier()
